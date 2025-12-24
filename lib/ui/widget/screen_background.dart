7899aaa55fc0177232530f7427d2d291a2d5ed5c
Mehadi-Hassan-Emon <mehadimd174@gmail.com>
2025-12-24 18:04:32 +0600

    screen_background.dart

--- lib/ui/widget/screen_background.dart
+++ lib/ui/widget/screen_background.dart
@@ -10,12 +10,12 @@ class ScreenBackground extends StatelessWidget {
   Widget build(BuildContext context) {
     return   Stack(
       children: [
-        SvgPicture.asset(AssetPaths.backgroundSvg,//assets_paths file theke paisi//svg picture add korte eita use hoi
+        SvgPicture.asset(AssetPaths.backgroundSvg,
            width:double.maxFinite,
            height:double.maxFinite,
           fit: BoxFit.cover,
         ),
-        SafeArea(child: child)         //child widget make korse karon holo onk somoi background e sudu image nah onk text o use hoi tai eibabe text and image 2 tai use kora jabe
+        SafeArea(child: child)         
       ],
     );
   }
