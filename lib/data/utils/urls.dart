f2c833e7df0bb8d63be79824250bbe502d4222af
Mehadi-Hassan-Emon <mehadimd174@gmail.com>
2025-12-24 17:54:02 +0600

    Update urls.dart

--- lib/data/utils/urls.dart
+++ lib/data/utils/urls.dart
@@ -1,7 +1,7 @@
 class Urls{
   static String _baseUrl = 'http://35.73.30.144:2005/api/v1';
 
-  static String registrationUrl  = '$_baseUrl/registration';//registration page er jonno url postman theke make kora
+  static String registrationUrl  = '$_baseUrl/registration';
   static String loginUrl  = '$_baseUrl/login';
    static String createTaskUrl  = '$_baseUrl/createTask';
   static String taskCountUrl  = '$_baseUrl/taskStatusCount';
@@ -14,13 +14,6 @@ class Urls{
 
 
 
-  static String changeStatus(String taskId,String status)   => '$_baseUrl/updateTaskStatus/$taskId/$status';               ///eta nisi screen e je Showdialog e page ta ashe oitar maddome onno page e select er sath sathe jate jai
-
-  //static String taskList(String type)   => '$_baseUrl/listTaskByStatus/$type';//jodi ekta url diye new,copleted,cancelled,progress kori tahole eibabe korbo
-
-
-
-
-
+  static String changeStatus(String taskId,String status)   => '$_baseUrl/updateTaskStatus/$taskId/$status';             
 
 }
