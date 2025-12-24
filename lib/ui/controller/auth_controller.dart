941ce545a3836732ba202cb0a6740122e6f27e2a
Mehadi-Hassan-Emon <mehadimd174@gmail.com>
2025-12-24 18:05:56 +0600

    auth_controller.dart

--- lib/ui/controller/auth_controller.dart
+++ lib/ui/controller/auth_controller.dart
@@ -8,34 +8,32 @@ class AuthController{
   static String _accessTokenKey  ='token';
   static String _userModelKey  ='user-data';
 
-  static String ? accessToken;//puro project e access korte keyword ta make korche
+  static String ? accessToken;
   static UserModel ? userModel;
 
-  static Future saveUserData(UserModel model,String token)async{//save kore rakhtasi token and usermodel ta
-    SharedPreferences sharedPreferences =await SharedPreferences.getInstance();//sharereference e token and Usermodel save rakhte hole pub e add korte hobe erpor ekhane ekta object make korte hobe
-    await sharedPreferences.setString(_accessTokenKey, token);//set korte setstring use hoi //token ta set korse
+  static Future saveUserData(UserModel model,String token)async{
+    SharedPreferences sharedPreferences =await SharedPreferences.getInstance();
+    await sharedPreferences.setString(_accessTokenKey, token);
     await sharedPreferences.setString(_accessTokenKey, jsonEncode(model.toJson()));
     accessToken  = token;
     userModel = model;
   }
   //access
-  static Future getUserData()async{//saveUserDatar data gula use korar jonno
+  static Future getUserData()async{
     SharedPreferences sharedPreferences =await SharedPreferences.getInstance();
-    String ? token= sharedPreferences.getString(_accessTokenKey);///get mane setString er data gula paoya
-////
-    if(token != null){
-      String ? userData =await sharedPreferences.getString(_userModelKey);//data ta get mane paitasi
-      userModel =UserModel.fromjson(jsonDecode(userData!));//usermodel theke je data ashbe oita api te pass kore dibo
-    }
-  }
-  ///
-  static Future<bool> isUserLogIn()async{//mainly amra logout korle shared preferance je data save kore rekhe de ta jate next time clear kore dei tai ei part ta
-    SharedPreferences sharedPreferences =await SharedPreferences.getInstance();//app login achr kina check korbo tai sharedpreferance e data save ache kina dekhtasi
-    String ? token= sharedPreferences.getString(_accessTokenKey);//data token thakle bujbo user ta login obothai ache
-    return token != null;//token null true hole login ache false hole login nai
+    String ? token= sharedPreferences.getString(_accessTokenKey);
 
+    if(token != null){
+      String ? userData =await sharedPreferences.getString(_userModelKey);
+      userModel =UserModel.fromjson(jsonDecode(userData!));
   }
-  static Future<void>clearUserData()async{//eibar amra logout er data gula check korbo
+  
+  static Future<bool> isUserLogIn()async{
+    SharedPreferences sharedPreferences =await SharedPreferences.getInstance();
+    String ? token= sharedPreferences.getString(_accessTokenKey);
+    return token != null;
+  }
+  static Future<void>clearUserData()async{
     SharedPreferences sharedPreferences =await SharedPreferences.getInstance();
     await sharedPreferences.clear();
   }
